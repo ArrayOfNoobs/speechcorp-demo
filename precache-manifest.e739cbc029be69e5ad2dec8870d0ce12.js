35e22fa1b8b07b3004f351a85a1142c61d19dbab
vsasvipul0605 <vsasvipul@gmail.com>
2020-07-21 13:34:08 +0530

    Updates

---
+++ precache-manifest.e739cbc029be69e5ad2dec8870d0ce12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9eb38f7fc44c0ae6669611a38d549798",
+    "url": "/speechcorp-demo/index.html"
+  },
+  {
+    "revision": "890518563119222fa42c",
+    "url": "/speechcorp-demo/static/css/main.2bd0cc6c.chunk.css"
+  },
+  {
+    "revision": "9c949adc3ff5c64879e7",
+    "url": "/speechcorp-demo/static/js/2.9a6a4ac9.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/speechcorp-demo/static/js/2.9a6a4ac9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "890518563119222fa42c",
+    "url": "/speechcorp-demo/static/js/main.d2e1b79a.chunk.js"
+  },
+  {
+    "revision": "ef3eec69d07cec06148c",
+    "url": "/speechcorp-demo/static/js/runtime-main.b3247148.js"
+  }
+]);
